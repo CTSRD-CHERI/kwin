a1bb446094a900b04bf3539b55e801a6604da4fd
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2022-02-21 11:53:10 +0000

    Fix the JSON conversion script
    Needs to use a KPlugin sub-key

--- src/effects/strip-effect-metadata.py
+++ src/effects/strip-effect-metadata.py
@@ -14,11 +14,11 @@ def main():
     parser.add_argument("--source", help="input file", required=True)
     parser.add_argument("--output", help="output file", required=True)
     args = parser.parse_args()
-    stripped_json = dict()
+    stripped_json = dict(KPlugin=dict())
     with open(args.source, "r") as src:
         original_json = json.load(src)
-        stripped_json["Id"] = original_json["KPlugin"]["Id"]
-        stripped_json["EnabledByDefault"] = original_json["KPlugin"]["EnabledByDefault"]
+        stripped_json["KPlugin"]["Id"] = original_json["KPlugin"]["Id"]
+        stripped_json["KPlugin"]["EnabledByDefault"] = original_json["KPlugin"]["EnabledByDefault"]
 
     with open(args.output, "w") as dst:
         json.dump(stripped_json, dst)
